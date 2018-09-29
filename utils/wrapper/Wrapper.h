a7bafe4dc316291f6e1c93a520d57313fb7b5bbc
NivenT <nachenjang@gmail.com>
2018-09-29 21:47:29 +0200

    constexpr template is a c++17 thing

--- utils/wrapper/Wrapper.h
+++ utils/wrapper/Wrapper.h
@@ -8,63 +8,59 @@
     template<typename T, typename U=T> \
     struct name##Exists { \
         enum { value = !::std::is_same<decltype(*(T*)(0) op *(U*)(0)), Nop>::value }; \
-    }; \
-    template<typename T, typename U=T> \
-    constexpr bool name##Exists_v = name##Exists<T, U>::value;
+    };
 
 #define UTILS_UNARY_OP_CHECKER(name, op) \
     template<typename T> Nop operator op(T&); \
     template<typename T> \
     struct name##Exists { \
         enum { value = !::std::is_same<decltype(op *(T*)(0)), Nop>::value }; \
-    }; \
-    template<typename T> \
-    constexpr bool name##Exists_v = name##Exists<T>::value;
+    };
 
 #define UTILS_IMPL_WRAPPER_OP(name, op)  \
-    template<typename U, typename std::enable_if_t<check:: name##Exists_v<T, U>>* = nullptr> \
+    template<typename U, typename std::enable_if_t<check:: name##Exists<T, U>::value>* = nullptr> \
     Wrapper operator op(const U& rhs) const { \
         return Wrapper(m_data op rhs);       \
     } \
-    template<typename U, typename std::enable_if_t<check:: name##Exists_v<T, U>>* = nullptr> \
+    template<typename U, typename std::enable_if_t<check:: name##Exists<T, U>::value>* = nullptr> \
     Wrapper& operator op##=(const U& rhs) { \
         m_data = m_data op rhs; \
         return *this; \
     }   
 
 #define UTILS_IMPL_WRAPPER_SELF_OP(name, op) \
-    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists_v<T>>* = nullptr> \
+    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists<T>::value>* = nullptr> \
     Wrapper<T, TAG> operator op(const Wrapper<T, TAG>& lhs, const Wrapper<T, TAG>& rhs) { \
         return Wrapper<T, TAG>(lhs.to_inner() op rhs.to_inner()); \
     } \
-    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists_v<T>>* = nullptr> \
+    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists<T>::value>* = nullptr> \
     Wrapper<T, TAG>& operator op##=(Wrapper<T, TAG>& lhs, const Wrapper<T, TAG>& rhs) { \
         lhs.to_inner() = lhs.to_inner() op rhs.to_inner(); \
         return Wrapper<T, TAG>(lhs); \
     }
 
 #define UTILS_IMPL_WRAPPER_UNARY_OP(name, op, c) \
-    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists_v<T>>* = nullptr> \
+    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists<T>::value>* = nullptr> \
     Wrapper<T, TAG> operator op(c Wrapper<T, TAG>& w) { \
         return Wrapper<T, TAG>(op w.to_inner()); \
     }
 
 #define UTILS_IMPL_WRAPPER_COMP(name, op) \
-    template<typename U, typename std::enable_if_t<check:: name##Exists_v<T, U>>* = nullptr> \
+    template<typename U, typename std::enable_if_t<check:: name##Exists<T, U>::value>* = nullptr> \
     bool operator op(const U& rhs) const { \
         return m_data op rhs; \
     } \
-    template<typename U, typename std::enable_if_t<check:: name##Exists_v<T, U> && check::EqualsExists_v<T, U>>* = nullptr> \
+    template<typename U, typename std::enable_if_t<check:: name##Exists<T, U>::value && check::EqualsExists<T, U>::value>* = nullptr> \
     bool operator op##=(const U& rhs) const { \
         return m_data op rhs || m_data == rhs; \
     }
 
 #define UTILS_IMPL_WRAPPER_SELF_COMP(name, op) \
-    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists_v<T>>* = nullptr> \
+    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists<T>::value>* = nullptr> \
     bool operator op(const Wrapper<T, TAG>& lhs, const Wrapper<T, TAG>& rhs) { \
         return lhs.to_inner() op rhs.to_inner(); \
     } \
-    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists_v<T> && check::EqualsExists_v<T>>* = nullptr> \
+    template<typename T, typename TAG, typename std::enable_if_t<check:: name##Exists<T>::value && check::EqualsExists<T>::value>* = nullptr> \
     bool operator op##=(const Wrapper<T, TAG>& lhs, const Wrapper<T, TAG>& rhs) { \
         return lhs.to_inner() op rhs.to_inner() || lhs.to_inner() == rhs.to_inner(); \
     }
@@ -110,7 +106,7 @@ namespace utils {
         auto operator==(const U& rhs) const -> decltype(std::enable_if_t<check::EqualsExists<T, U>::value, bool>{}) {
             return m_data == rhs;
         }
-        template<typename std::enable_if_t<check::EqualsExists_v<T>>* = nullptr>
+        template<typename std::enable_if_t<check::EqualsExists<T>::value>* = nullptr>
         bool operator==(const Wrapper& rhs) const {
             return m_data == rhs.m_data;
         }
@@ -118,7 +114,7 @@ namespace utils {
         auto operator!=(const U& rhs) const -> decltype(std::enable_if_t<check::EqualsExists<T, U>::value, bool>{}) {
             return m_data != rhs;
         }
-        template<typename std::enable_if_t<check::EqualsExists_v<T>>* = nullptr>
+        template<typename std::enable_if_t<check::EqualsExists<T>::value>* = nullptr>
         bool operator!=(const Wrapper& rhs) const {
             return !(m_data == rhs.m_data);
         }
@@ -148,20 +144,20 @@ namespace utils {
     UTILS_IMPL_WRAPPER_UNARY_OP(Neg, -, const)
     UTILS_IMPL_WRAPPER_UNARY_OP(Not, ~, const)
     UTILS_IMPL_WRAPPER_UNARY_OP(Incr, ++,) // pre-incrementation
-    template<typename T, typename TAG, typename std::enable_if_t<check::IncrExists_v<T>>* = nullptr>
+    template<typename T, typename TAG, typename std::enable_if_t<check::IncrExists<T>::value>* = nullptr>
     Wrapper<T, TAG> operator++(Wrapper<T, TAG>& lhs, int) {
         auto ret = lhs;
         ++lhs;
         return ret;
     }
     UTILS_IMPL_WRAPPER_UNARY_OP(Decr, --,) // pre-decrementation
-    template<typename T, typename TAG, typename std::enable_if_t<check::DecrExists_v<T>>* = nullptr>
+    template<typename T, typename TAG, typename std::enable_if_t<check::DecrExists<T>::value>* = nullptr>
     Wrapper<T, TAG> operator--(Wrapper<T, TAG>& lhs, int) {
         auto ret = lhs;
         --lhs;
         return ret;
     }
-    template<typename T, typename TAG, typename std::enable_if_t<check::LShiftExists_v<std::ostream, T>>* = nullptr>
+    template<typename T, typename TAG, typename std::enable_if_t<check::LShiftExists<std::ostream, T>::value>* = nullptr>
     std::ostream& operator<<(std::ostream& lhs, const Wrapper<T, TAG>& w) {
         return lhs<<w.to_inner();
     }
