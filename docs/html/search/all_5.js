b665fc10add0366b4a03ab8dd534cc27a3dee72b
NivenT <nachenjang@gmail.com>
2018-09-30 14:55:22 +0200

    updated docs

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['file_5fname',['file_name',['../d9/d63/classutils_1_1Path.html#a7674aed071c675921184e9cad1f18848',1,'utils::Path']]],
+  ['file_5fname',['file_name',['../d9/d63/classutils_1_1Path.html#a6d09c3592c4ac5c0a8645a1835d0d34f',1,'utils::Path']]],
   ['find',['find',['../d6/d89/classutils_1_1TypeMap.html#aca80b27410a714c3fe63fa5e2c5bf2b5',1,'utils::TypeMap::find() const'],['../d6/d89/classutils_1_1TypeMap.html#ae874e214372ca6dc87af24881317f7e6',1,'utils::TypeMap::find(const TypeInfo &amp;info) const']]],
   ['flags',['flags',['../d1/dc9/structutils_1_1TypeInfo.html#a2eb3c7a518d5a985b4e2f5e10ef77697',1,'utils::TypeInfo']]],
   ['from_5ffile',['from_file',['../de/df3/classutils_1_1Json.html#a58e372e88f87b45315835f2dd18325d9',1,'utils::Json']]],
