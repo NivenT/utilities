97f19b5e61654866e3efd5eb7ebc99d68c42536f
NivenT <nachenjang@gmail.com>
2018-10-12 14:41:46 +0200

    Updated docs

--- docs/html/search/classes_d.js
+++ docs/html/search/classes_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setbeginendkey',['SetBeginEndKey',['../df/d7b/classutils_1_1Json_1_1SetBeginEndKey.html',1,'utils::Json::SetBeginEndKey'],['../d4/d90/classutils_1_1Path_1_1SetBeginEndKey.html',1,'utils::Path::SetBeginEndKey']]],
+  ['strongwrapper',['StrongWrapper',['../d4/d2d/classutils_1_1StrongWrapper.html',1,'utils']]],
   ['subexists',['SubExists',['../dc/d42/structutils_1_1check_1_1SubExists.html',1,'utils::check']]]
 ];
